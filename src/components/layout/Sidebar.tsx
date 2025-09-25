59ff659abb247a7b281a981a4cc3de52a36154a0
Sachin <sachinlakshan04@gmail.com>
2025-09-25 16:35:05 +0530

    fix layout issue

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -1,6 +1,6 @@
 const Sidebar = () => {
   return (
-    <div className="hidden lg:block mt-15 lg:w-[220px] xl:w-[250px] bg-muted/50 shadow-lg border-r sticky top-0 h-screen">
+    <div className="hidden lg:block lg:w-[220px] xl:w-[250px] bg-muted/50 shadow-lg border-r fixed top-15 h-screen">
       Sidebar
     </div>
   );
