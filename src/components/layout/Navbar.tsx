1260db09738eae43a88393b9c605a4a0dc36595f
Sachin <sachinlakshan04@gmail.com>
2025-09-28 00:44:35 +0530

    fix the navbar

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,13 +1,14 @@
 import logo from "@/assets/logo.webp";
 import { ModeToggle } from "../shared/mode-toggle";
+import { Link } from "react-router";
 
 const Navbar = () => {
   return (
     <div className="h-15 px-4 flex items-center justify-between shadow-md border-b fixed top-0 left-0 right-0 bg-white dark:bg-black z-50">
-      <div className="flex items-center">
+      <Link to={"/"} className="flex items-center">
         <img src={logo} className="h-15 object-cover" />
         <p className="uppercase tracking-widest font-extrabold">Arcadia</p>
-      </div>
+      </Link>
       <ModeToggle />
     </div>
   );
